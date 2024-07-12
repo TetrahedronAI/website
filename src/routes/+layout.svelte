2fc3acbc941d75fd43050f15337f70fa54d5b82a
sam-the-programmer <85027459+sam-the-programmer@users.noreply.github.com>
2024-07-12 10:04:11 +0100

    commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import Navbar from "$lib/ui/Navbar.svelte";
-  import "../app.postcss"
+  import "../app.postcss";
+
+  // import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+
+  // injectSpeedInsights();
 </script>
 
 <Navbar />
